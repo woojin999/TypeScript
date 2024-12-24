ab64827c6f2b19a05f085b43f00e17613700f171
LEE WOOJIN <24864@naver.com>
2024-12-24 11:37:21 +0900

    Create 1_type-basic.ts

---
+++ learn-typescript/class-note/1_type-basic.ts
@@ -0,0 +1,12 @@
+// js 문자열 선언
+// let str = 'hello';
+
+// ts 문자열 선언
+let str: string = "hello";
+let num: number = 10;
+let arr: Array<number> = [1, 2, 3];
+let heroes: Array<string> = ["Capt", "Thor", "Hulk"];
+let items: number[] = [1,2,3];
+
+// ts 튜플
+let address: [string,number] = ['gangnam',10];
